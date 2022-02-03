fdb48da4e35ce2cf8b34059e83a0000c4ad09630
avpaz <avpaz@microsoft.com>
2022-02-03 16:03:23 +0200

    adding tabs

--- azure-webapp/src/azure-storage/components/ContainerList.tsx
+++ azure-webapp/src/azure-storage/components/ContainerList.tsx
@@ -26,17 +26,6 @@ const ContainerList: React.FC = () => {
            <button onClick={() => onContainerClick(item.name)}>View/Upload</button>
         </div>
       ))}
-
-    Reports    
-    <button onClick={()=> window.open("https://msit.powerbi.com/groups/me/reports/015a962a-5c80-4bf6-8c3f-04cc42c96bae/ReportSection", "_blank")}>Visualization</button>
-      {/* <iframe 
-          title="ta4h - Page 1" 
-          width="1140" 
-          height="541.25" 
-          src="https://msit.powerbi.com/reportEmbed?reportId=015a962a-5c80-4bf6-8c3f-04cc42c96bae&autoAuth=true&ctid=72f988bf-86f1-41af-91ab-2d7cd011db47&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly9kZi1tc2l0LXNjdXMtcmVkaXJlY3QuYW5hbHlzaXMud2luZG93cy5uZXQvIn0%3D" 
-          frameBorder="0" 
-          allowFullScreen>
-      </iframe> */}
     </div>
   );
 };
