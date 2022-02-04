a7369a97235b297a4be1c2bf20f16dbc8e0628a8
orbarnea <rp35iit3tfkawen7pwn53ptczfxzyerzykadm2oeyu2h7k75brgq>
2022-02-04 11:58:40 -0800

    adding fluent and breaking to minor components, moving server to root and updating several dependencies

--- azure-webapp/src/layout/UploadTab.tsx
+++ azure-webapp/src/layout/UploadTab.tsx
@@ -4,21 +4,32 @@ import ItemsList from "../azure-storage/components/ItemsList";
 import ItemsUploaded from "../azure-storage/components/ItemsUploaded";
 import ItemsDownloaded from "../azure-storage/components/ItemsDownloaded";
 import ItemsDeleted from "../azure-storage/components/ItemsDeleted";
+import { ScrollablePane } from "@fluentui/react";
 
 const UploadTab: React.FC = () => {
     return (
         <>
             <h2>Upload medical documents</h2>
             <SelectedContainer className="container">
-                <ItemsList />
+                <div style={{
+                    height: "40vh",
+                    position: 'relative',
+                }}>
+                    <ScrollablePane>
+
+                        <ItemsList/>
+                    </ScrollablePane>
+                </div>
+
                 <div className="item-details">
-                    <ItemsUploaded />
-                    <ItemsDownloaded />
-                    <ItemsDeleted />
+                    <ItemsUploaded/>
+                    <ItemsDownloaded/>
+                    <ItemsDeleted/>
                 </div>
             </SelectedContainer>
-
         </>
+
+
     );
 };
 
