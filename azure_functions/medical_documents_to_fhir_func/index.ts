e076bef73fd3e02a6205114841814c05d2bdaefc
avpaz <avpaz@microsoft.com>
2022-02-27 17:41:59 +0200

    adding FHIR structuring support

--- azure_functions/medical_documents_to_fhir_func/index.ts
+++ azure_functions/medical_documents_to_fhir_func/index.ts
@@ -1,4 +1,5 @@
 import { AzureFunction, Context } from "@azure/functions";
+import {response} from "express";
 const axios = require('axios').default;
 
 
@@ -17,17 +18,23 @@ const blobTrigger: AzureFunction = async function (context: Context, myBlob: any
     }));
 
     const endpoint = process.env.TA_FHIR_STRUCTURING_ENDPOINT;
-    const config = { headers: { 'Content-Type': 'application/json' }};
-
+    const config = {
+        headers: { 'content-Type': 'application/json' },
+        params:  { structureFHIR: true }
+    };
+    // const data = {body: { documents: documents }};
+    const data = { "documents": documents };
     try {
-        const data = {body: { documents: documents }};
         const resp = await axios.post(endpoint, data, config);
         context.log(resp);
-        context.log(JSON.parse(resp));
     }
     catch (err) {
         context.log(err);
     }
+
+
+    const fhirDocs = response.data.documents.map(d => JSON.parse(d.fhirBundle));
+    context.log(fhirDocs);
 }
 
 export default blobTrigger;
