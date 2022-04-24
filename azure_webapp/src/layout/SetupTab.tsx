6e92289fa705c85f4f4cac453db1385fb37c4a26
avpaz <avpaz@microsoft.com>
2022-04-24 21:54:24 +0300

    cosmetics

--- azure_webapp/src/layout/SetupTab.tsx
+++ azure_webapp/src/layout/SetupTab.tsx
@@ -8,14 +8,17 @@ const SetupTab: React.FC = () => {
             <ol>
                 <li>
                     <Text>Download the following <Link
-                        href="https://github.com/pazinio/Health-AI-Blueprints-TA4HVisualizer/raw/main/powerbi/ta4h.pbix"
+                        href="https://github.com/microsoft/Health-AI-Blueprints-TA4HVisualizer/raw/main/powerbi/ta4h.pbix"
                         target={"_blank"}>PBIX file</Link> </Text>
                 </li>
                 <li>
-                    <Text>Publish it using Power BI desktop</Text>
+                    <Text>Edit and adjust it according to your requirements and publish it using Power BI desktop</Text>
                 </li>
                 <li>
                     <Text>Change FHIR Server connection settings</Text>
+                </li>
+                <li>
+                    <Text>Edit VisualizeTab.tsx file to embed your Power BI reports within the Visualizer App</Text>
                 </li>
                 <li>
                     <Text>Power BI Premium License is required to embed reports within the Visualizer App</Text>
