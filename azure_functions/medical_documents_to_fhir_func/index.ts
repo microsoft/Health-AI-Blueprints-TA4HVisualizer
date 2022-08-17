336a7f510e53416d202f42e57233c6fc2ad2824b
asafmic <106982692+asafmic@users.noreply.github.com>
2022-08-17 15:09:51 +0300

    Update index.ts
    make it async + calling "language" resource

--- azure_functions/medical_documents_to_fhir_func/index.ts
+++ azure_functions/medical_documents_to_fhir_func/index.ts
@@ -2,6 +2,10 @@ import { AzureFunction, Context } from "@azure/functions";
 const axios = require('axios').default;
 
 
+function delay(milliseconds : number) {
+    return new Promise(resolve => setTimeout( resolve, milliseconds));
+}
+
 const blobTrigger: AzureFunction = async function (context: Context, myBlob: any): Promise<void> {
     context.log("Blob trigger function processed blob \n Name:", context.bindingData.name, "\n Blob Size:", myBlob.length, "Bytes");
 
@@ -16,39 +20,84 @@ const blobTrigger: AzureFunction = async function (context: Context, myBlob: any
         isAutoLanguageDetectionEnabled: false
     }));
 
-    const structuringEndpoint = 'https://' + process.env.TA_FHIR_STRUCTURING_RESOURCE_NAME + '.cognitiveservices.azure.com/language/analyze-text/jobs?api-version=2022-04-01-preview';
+    const structuringEndpoint = 'https://' + process.env.TA_FHIR_STRUCTURING_RESOURCE_NAME + '.cognitiveservices.azure.com/language/analyze-text/jobs';
     const config = {
         headers: {
             'content-Type': 'application/json',
-            'Ocp-Apim-Subscription-Key': process.env.TA_API_KEY
+            'Ocp-Apim-Subscription-Key': process.env.TA_API_KEY,
+            'apim-subscription-id': 'abc'
         },
-        params:  { structureFHIR: true }
+        params: {
+            'api-version': process.env.TA_API_VERSION
+        }
     };
     // const data = {body: { documents: documents }};
 
-    let fhirDocs = [];
-    const data = { "documents": documents };
+    var fhirDoc;
+    const data = {
+        "tasks": [
+            {
+                "kind": "Healthcare",
+                "parameters": {
+                    "modelVersion": "latest",
+                    "fhirVersion": process.env.TA_FHIR_API_VERSION
+                }
+            }
+        ],
+        "analysisInput": {
+            "documents": documents
+        }
+    };
+
     try {
+        console.log( "sending structuring request");
         const resp = await axios.post(structuringEndpoint, data, config);
         context.log(resp);
-        fhirDocs = resp.data.documents.map(d => JSON.parse(d.fhirBundle));
+        let getUrlRaw = resp.headers['operation-location'];
+        let getUrl = getUrlRaw.split("?",2)[0];
+        console.log( "getUrl=" + getUrl);
+        console.log( "getUrlRaw=" + getUrlRaw);
+
+        var respGet;
+        let i: number = 0;
+        while (i < 10) {
+            console.log( "trying to fetch structuring result. try:  " + i )
+            respGet = await axios.get(getUrl, config);
+            if (respGet.data.tasks.completed == '1')
+                break;
+            console.log( "structuring not completed");
+            console.log( "respGet.data.tasks.completed =" + respGet.data.tasks.completed );
+            await delay(500);
+
+            i++;
+        }
+        if (i == 10) {
+            console.log("structuring failed");
+        }
+        else {
+            console.log("structuring succeeded");
+            let text = respGet.data.tasks.items[0].results.documents[0].fhirBundle;
+            let temp = JSON.stringify(text);
+            fhirDoc = JSON.parse(temp);
+            // fhirDocs = respGet.data.tasks.items[0].results.documents.map(d => JSON.parse(d.fhirBundle));
+        }
     }
     catch (err) {
         context.log(err);
         throw err;
     }
 
-    context.log(fhirDocs);
+    context.log(fhirDoc);
 
-    if (fhirDocs.length == 0) {
+    if (fhirDoc == null) {
         context.done();
         return;
     }
 
-    context.bindings.outputBlob = JSON.stringify(fhirDocs[0], null, 4);
+    context.bindings.outputBlob = JSON.stringify(fhirDoc, null, 4);
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: fhirDocs[0]
+        body: fhirDoc
     };
 
 }
