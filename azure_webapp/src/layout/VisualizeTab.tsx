398ed482bba3b92644fc168d4b02fd6d31eb99b9
asafmic <106982692+asafmic@users.noreply.github.com>
2022-08-31 17:00:38 +0300

    Update VisualizeTab.tsx

--- azure_webapp/src/layout/VisualizeTab.tsx
+++ azure_webapp/src/layout/VisualizeTab.tsx
@@ -6,7 +6,7 @@ const VisualizeTab: React.FC = () => {
         <>
             <Stack horizontal horizontalAlign={"space-between"} verticalAlign={"center"}>
                 <h2>Reports</h2>
-                <Link href={"https://msit.powerbi.com/links/kOk-vQai7G?ctid=72f988bf-86f1-41af-91ab-2d7cd011db47&pbi_source=linkShare"} target={"_blank"}>Open in PowerBI</Link>
+                <Link href={"https://msit.powerbi.com/links/kOk-vQai7G?ctid=72f988bf-86f1-41af-91ab-2d7cd011db47&pbi_source=linkShare"} target={"_blank"}>Open in Power BI</Link>
             </Stack>
 
             {
