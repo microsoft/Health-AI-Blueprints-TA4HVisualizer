3f8aa8aa40cb5c0c46f0672ef403524ce832056e
asafmic <106982692+asafmic@users.noreply.github.com>
2022-08-31 16:40:54 +0300

    Update VisualizeTab.tsx

--- azure_webapp/src/layout/VisualizeTab.tsx
+++ azure_webapp/src/layout/VisualizeTab.tsx
@@ -5,13 +5,13 @@ const VisualizeTab: React.FC = () => {
     return (
         <>
             <Stack horizontal horizontalAlign={"space-between"} verticalAlign={"center"}>
-                <h2>Reportss</h2>
-                <Link href={process.env.REPORT_URL_POWERBI_1} target={"_blank"}>1111Open in Power BII</Link>
+                <h2>Reports</h2>
+                <Link href={"https://msit.powerbi.com/links/kOk-vQai7G?ctid=72f988bf-86f1-41af-91ab-2d7cd011db47&pbi_source=linkShare"} target={"_blank"}>Open in Power BI</Link>
             </Stack>
 
             {
             <iframe title="Clinical Insights_FHIR" width="1140" height="541.25"
-                        src=process.env.REPORT_URL_1
+                        src="https://msit.powerbi.com/reportEmbed?reportId=81d3ba53-2dad-4072-9ad2-8a0a47e43be6&autoAuth=true&ctid=72f988bf-86f1-41af-91ab-2d7cd011db47"
                         frameBorder="0" allowFullScreen></iframe>
             }
         </>
